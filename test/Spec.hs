cd48097a1b1bd872dc5a1d32ff6ac659761844ba
card <cardenaso11@users.noreply.github.com>
2025-11-06 11:21:52 +0000

    Make tests more concise

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,7 +8,7 @@ import qualified Data.Map.Strict as Map
 import Reflex.Dom.Attrs
 import Reflex.Spider.Internal (Global, SpiderTimeline)
 import Test.Tasty (TestTree, defaultMain, testGroup)
-import Test.Tasty.HUnit (testCase, (@?=))
+import Test.Tasty.HUnit (testCase, assertEqual)
 
 newtype TestM a = TestM {runTestM :: Identity a}
   deriving (Functor, Applicative, Monad)
@@ -17,7 +17,7 @@ type TestTimeline = SpiderTimeline Global
 
 testLastStyleWins :: TestTree
 testLastStyleWins =
-  testCase "when mconcat combines Attrs that set a CSS property, the right-most conflicting property set wins" $
+  testCase "when mconcat combines Attrs that set a CSS property, the right-most conflicting property set wins" $ do
     let combinedAttr :: Attrs TestTimeline TestM
         combinedAttr = "style" ~:
           [ "color" ~:: "red"
@@ -25,22 +25,11 @@ testLastStyleWins =
           , "color" ~:: "blue"
           ]
         finalColor = Map.lookup "color" (attrs_style combinedAttr)
-    in finalColor @?= Just "blue"
-
-testFirstStyleLoses :: TestTree
-testFirstStyleLoses = 
-  testCase "when mconcat combines Attrs that set a CSS property, the left-most conflicting property set loses" $
-    let combinedAttr :: Attrs TestTimeline TestM
-        combinedAttr = "style" ~:
-          [ "color" ~:: "red"
-          , "color" ~:: "green"
-          , "color" ~:: "blue"
-          ]
-        finalColor = Map.lookup "color" (attrs_style combinedAttr)
-    in (finalColor == Just "red") @?= False
+    assertEqual "final property is the rightmost" finalColor $ Just "blue"
+    assertEqual "final property is not the leftmost" (finalColor == Just "red") False
 
 testSemigroup :: TestTree
-testSemigroup = testGroup "Attrs t m Semigroup instance" [testFirstStyleLoses, testLastStyleWins]
+testSemigroup = testGroup "Attrs t m Semigroup instance" [testLastStyleWins]
 
 main :: IO ()
 main =
